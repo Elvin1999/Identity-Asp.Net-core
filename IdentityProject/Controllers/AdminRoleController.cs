00e8cd2eccf5ce2f218b9272aba26b34f500e615
camalzade_elvin <camalzade_elvin@outlook.com>
2020-01-04 16:33:06 -0800

    i add role management to project but not Authorization . . .

--- IdentityProject/Controllers/AdminRoleController.cs
+++ IdentityProject/Controllers/AdminRoleController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IdentityProject.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,14 +11,18 @@ namespace IdentityProject.Controllers
     public class AdminRoleController : Controller
     {
         private RoleManager<IdentityRole> roleManager;
+        private UserManager<ApplicationUser> userManager;
 
-        public AdminRoleController(RoleManager<IdentityRole> roleManager)
+        public AdminRoleController(RoleManager<IdentityRole> roleManager,
+            UserManager<ApplicationUser> userManager)
         {
             this.roleManager = roleManager;
+            this.userManager = userManager;
         }
 
         public IActionResult Index()
         {
+            
             return View(roleManager.Roles);
         }
         [HttpGet]
@@ -45,6 +50,94 @@ namespace IdentityProject.Controllers
             }
              return View(name);
             
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(RoleEditModel model)
+        {
+            IdentityResult result;
+            if (ModelState.IsValid)
+            {
+                foreach (var userId in model.IdsToAdd ?? new string[] { })
+                {
+                    var user = await userManager.FindByIdAsync(userId);
+                    if (user != null)
+                    {
+                        result = await userManager.AddToRoleAsync(user, model.RoleName);
+                        if (!result.Succeeded)
+                        {
+                            foreach (var item in result.Errors)
+                            {
+                                ModelState.AddModelError("", item.Description);
+                            }
+                        }
+                    }
+                }
+
+                foreach (var userId in model.IdsToDelete ??new string[] { })
+                {
+                    var user = await userManager.FindByIdAsync(userId);
+                    if (user != null)
+                    {
+                        result = await userManager.RemoveFromRoleAsync(user, model.RoleName);
+                        if (!result.Succeeded)
+                        {
+                            foreach (var item in result.Errors)
+                            {
+                                ModelState.AddModelError("", item.Description);
+                            }
+                        }
+                    }
+                }
+                if (ModelState.IsValid)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return RedirectToAction("Edit", model.RoleId);
+                }
+            }
+            return RedirectToAction("Index");
+        }
+        public async Task<IActionResult> Edit(string id)
+        {
+            var role = await roleManager.FindByIdAsync(id);
+            var members = new List<ApplicationUser>();
+            var nonmembers = new List<ApplicationUser>();
+            foreach (var user in userManager.Users)
+            {
+                var list = await userManager.IsInRoleAsync(user, role.Name)
+                    ? members : nonmembers;
+                    list.Add(user);
+            }
+            var model = new RoleDetails() { 
+             Role=role,
+              Members=members,
+               NonMembers=nonmembers
+            };
+            return View(model);
+        }
+        public async Task<IActionResult> Delete(string id)
+        {
+            var role = await roleManager.FindByIdAsync(id);
+            if (role != null)
+            {
+                var result = await roleManager.DeleteAsync(role);
+                if (result.Succeeded)
+                {
+                    TempData["message"] = $"{role.Name} has been deleted";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+
+            }
+                return RedirectToAction("Index");
         }
     }
 }
